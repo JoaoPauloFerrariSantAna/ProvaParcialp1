a782939cd00ec57302e4eaa0bf9fd74b864061c9
JoaoPauloFerrariSantAna <94869217+JoaoPauloFerrariSantAna@users.noreply.github.com>
2025-09-03 22:13:03 -0300

    Add files via upload

---
+++ InventoryManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ApiProvaParcial
+{
+    public class InventoryManager
+    {
+        private List<Product> productList { get; set; }
+
+        public InventoryManager(List<Product> productList)
+        {
+            this.productList = productList;
+        }
+
+        public void AddProduct(Product product)
+        {
+            this.productList.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            if (!this.productList.Contains(product))
+            {
+                Console.WriteLine("item does not exists, so i cant erased");
+                return;
+            }
+
+            this.productList.Remove(product);
+        }
+
+        public List<Product> ConsultStock()
+        {
+            return this.productList;
+        }
+    }
+}
